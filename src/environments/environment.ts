1cb898ec50a6533632d52bbcc52080e8cbbcc4bc
Anil kumar Parajapati <102989827+Anil-Prajapati@users.noreply.github.com>
2026-02-04 19:13:10 +0530

    Update API URL in environment configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,1 @@
-export const environment: { production: boolean, apiUrl: string } = {production: false,apiUrl: 'http://localhost:8080/api'};
+export const environment: { production: boolean, apiUrl: string } = {production: false,apiUrl: 'https://clinicexpert-backend.onrender.com/api'};

f406b96fb4fc4131386f0e76ec53b28d19766594
Anil kumar Parajapati <102989827+Anil-Prajapati@users.noreply.github.com>
2026-02-05 17:10:16 +0530

    Update apiUrl to include '/api' endpoint

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,1 @@
-export const environment:{ production: boolean, apiUrl: string } = {production: true, apiUrl: 'https://clinicexpert-backend.onrender.com'};
+export const environment:{ production: boolean, apiUrl: string } = {production: true, apiUrl: 'https://clinicexpert-backend.onrender.com/api'};
